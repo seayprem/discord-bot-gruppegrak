77ab7e787551f1de179ac5784207480f5ce50fb2
Wanchai Saelim <seayprem@hotmail.com>
2021-02-28 08:29:14 +0000

    update

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ client = discord.Client()
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
-  @client.event
-  async def on_message():
-    if message.author == client.user:
-      return
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('$hello'):
+    await message.channel.send('เฉาก๊วยชากังราว!')
 
-    if message.content.startWith('$hello'):
-      await message.channel.send('สำหรับท่านที่เดินผ่านไปผ่านมาแวะมาชิมกันก่อนได้นะครับ')
-
-  client.run(os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN'))
